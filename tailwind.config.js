4262da36fce057f4c12871fa9730f1ee99c48bc5
MaryMumladze <mmumladzemari12@gmail.com>
2023-12-04 13:26:27 +0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: { 
+      fontFamily: {
+      Poppins: "Poppins",
+    },
+      colors: {
+        yellow: '#F6D215'
+      },
+     
+    },
+  },
+  plugins: [],
+};
